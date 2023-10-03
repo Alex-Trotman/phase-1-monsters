65b194433ea013d06cdf0a72aaf118d58b4fb9f8
Alex Trotman <alextrotman04@gmail.com>
2023-10-03 12:07:46 -0500

    finish

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 // Event listener added to the document object, listening for 'DOMContentLoaded'
 document.addEventListener("DOMContentLoaded", (event) => {
+  let currentPage = 1; // Initialize the current page
+
   // Grab createMonster div element
   const createMonsterDiv = document.getElementById("create-monster");
   // Create input for name
@@ -21,8 +23,43 @@ document.addEventListener("DOMContentLoaded", (event) => {
   createMonsterDiv.appendChild(inputDescription);
   createMonsterDiv.appendChild(inputButton);
 
+  inputButton.addEventListener("click", createMonsterFn);
+  function createMonsterFn() {
+    console.log("Button clicked!");
+
+    let name = inputName.value;
+    let age = inputAge.value;
+    let desc = inputDescription.value;
+
+    fetch("http://localhost:3000/monsters", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({ name: name, age: age, description: desc }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `Network response was not ok (${response.status} - ${response.statusText})`
+          );
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // Handle the successful response here
+        console.log("Successfully created a monster:", data);
+      })
+      .catch((error) => {
+        // Handle errors here
+        console.error("There was an error:", error);
+      });
+  }
+
   //   Fetch monsters.json
-  fetch("http://localhost:3000/monsters/?_limit=50&_page=0")
+  function loadMonsters(page){
+    fetch(`http://localhost:3000/monsters/?_limit=50&_page=${page}`)
     .then((response) => response.json())
     .then((data) => {
       // Console.log the data
@@ -44,4 +81,21 @@ document.addEventListener("DOMContentLoaded", (event) => {
         mosterContainer.appendChild(card);
       });
     });
+  }
+  // Load monsters when the page loads
+  loadMonsters(currentPage);
+
+  // Event listener for the "Load Previous" button
+  document.getElementById("back").addEventListener("click", () => {
+    if (currentPage > 1) {
+      currentPage--;
+      loadMonsters(currentPage);
+    }
+  });
+
+  // Event listener for the "Load Next" button
+  document.getElementById("load-next").addEventListener("click", () => {
+    currentPage++;
+    loadMonsters(currentPage);
+  });
 });
